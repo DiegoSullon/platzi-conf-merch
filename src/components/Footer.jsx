6ddd5811aa019e2675438be1bdaf770be2975a65
DiegoSullon <jdiego.sullon@gmail.com>
2021-09-27 13:58:18 -0500

    styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+      <p className='Footer-title'>PlatziConf Merch</p>
+      <p className='Footer-copy'>Todos los zurdos reservados</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='Header'>
+      <h1 className='Header-title'>PlatziConf Merch</h1>
+      <div className='Header-checkout'>Checkput</div>
+    </div>
+  );
+};
+
+export default Header;
