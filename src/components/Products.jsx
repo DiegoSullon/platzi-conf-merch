b6e28c9f35c18a087cb7f0edf515b6310bd94144
DiegoSullon <jdiego.sullon@gmail.com>
2021-09-29 23:30:50 -0500

    addToCart useContext

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Product from './Product';
 import '../styles/components/Products.css';
+import AppContext from '../context/AppContext';
 
-const Products = ({ products }) => {
+const Products = () => {
+  const { state, addToCart } = useContext(AppContext);
+  const { products } = state;
+
+  const handleAddToCart = product => () => {
+    addToCart(product);
+  };
   return (
     <div className='Products'>
       <div className='Products-items'>
-        {products && products.map(p => <Product key={p.id} product={p} />)}
+        {products &&
+          products.map(p => (
+            <Product key={p.id} product={p} handleAddToCart={handleAddToCart} />
+          ))}
       </div>
     </div>
   );
