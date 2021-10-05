8b9829aba547d50348f2083db44382ab5d80811b
DiegoSullon <jdiego.sullon@gmail.com>
2021-10-04 20:12:11 -0500

    esLint Fix

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import initialState from '../../initialState';
 import Products from '../components/Products';
 
 const Home = () => (
